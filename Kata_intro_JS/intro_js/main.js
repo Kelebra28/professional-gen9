8d203de2c6fd31e34310436c5372c7f4a86986d4
Kelebra <ricardokelebra@gmail.com>
2021-06-28 19:55:59 -0500

    intro a js

---
+++ Kata_intro_JS/intro_js/main.js
@@ -0,0 +1,60 @@
+// alert('!!!!!!!!!Descaga ya!!!!!!!!!!!')
+
+// ---------------> variables <-------------------
+// var -> nombre de la varibale = -> valor de la varibale
+var saludo = 'hola'
+
+// -----------------> tipos de datos <-----------------
+
+// string o texto 
+var nombre = 'Ricardo'
+var nombre2 = "Raul"
+
+// number
+var numero = 10
+var dedos = 20
+var pi = 3.14167981238914891498148412
+
+// boolean o boleanos
+
+var lluvia = true
+var solesito = false
+
+// -------> datos espeaciales <--------
+// array o arreglos
+ var animales = []
+
+ // object o objetos 
+ var musica = {}
+
+
+ // operadores matematicos simples
+//     +
+//     -
+//     /
+//     *
+
+var num1 = 50
+var num2 = 80
+var num3 = 100
+var sum = num1 + num2
+
+
+// ------> Outputs o salidas <------
+
+// alert('Que onda que pez')  ->>>> no lo hagan plox >:/
+console.log()
+console.log('Hola desde la consola')
+console.log('Hola mundo xd')
+
+console.log(sum)
+console.log(sum + num3)
+
+console.log(nombre + ' ' +nombre2)
+
+
+
+//  -------->Input o entrada <--------
+
+var pregunta = prompt('Ingresa tu nombre')
+console.log(pregunta)
