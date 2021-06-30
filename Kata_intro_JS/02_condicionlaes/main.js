ca86110f01483585bfbfccffb009841cbfceaa03
Kelebra <ricardokelebra@gmail.com>
2021-06-29 21:11:38 -0500

    operadores logicos

--- Kata_intro_JS/02_condicionlaes/main.js
+++ Kata_intro_JS/02_condicionlaes/main.js
@@ -22,21 +22,21 @@
 
 var numero = 12
 
-if ( numero !== '10') {
-    console.log('Es verdadero')
-} else  {
-    console.log('Es falso')
-}
+// if ( numero !== '10') {
+//     console.log('Es verdadero')
+// } else  {
+//     console.log('Es falso')
+// }
 
 //   Modulo -> %  -> residuo de la division 
 
-if (numero === 10) {
-    console.log('Es identico a 10')
-}else if (numero === 11) {
-    console.log('Es identico a 11')
-}else {
-    console.log('No es ni 10 ni 11')
-}
+// if (numero === 10) {
+//     console.log('Es identico a 10')
+// }else if (numero === 11) {
+//     console.log('Es identico a 11')
+// }else {
+//     console.log('No es ni 10 ni 11')
+// }
 
 
 // --------- Challenge 1 -----------
@@ -46,3 +46,53 @@ if (numero === 10) {
 // 1: impar
 // 2: par
 // Que pasa si el usuario pone letra x error 
+
+
+// var askNumber = prompt('Ingresa un numero para saber si es par o inpar')
+
+// // --> saber el tipo de dato --> typeof()
+// // console.log(typeof(askNumber))
+
+// // --> pasar el texto a tipo numerico
+// var valueNumber = Number(askNumber)
+
+// if (valueNumber % 2 === 0) {
+//     console.log('El numero: ' + valueNumber + ' es par')
+// } else if (valueNumber % 2 === 1) {
+//     console.log('El numero: ' + valueNumber + ' es inpar')
+// }else {
+//     console.error('No es numero, intentalo de nuevo')
+// }
+
+
+
+// --------------> Operadores Logicos <--------------
+
+//   &&  --> and 
+//   ||  --> or 
+
+
+// Si puedes conducir
+// es menor de edad -> no puede conducir
+// si 16 y 17  -> permiso para conducir
+// si eres mayor de 18 puedes conducir 
+// si si eres mayor de 90  --> ya no puedes conducir
+
+var askAge = prompt('Cual es tu edad')
+var valueAge = Number(askAge)
+
+if (valueAge >= 18 && valueAge <= 90) {
+    console.log('Puedes conducir 🤓')
+} else if (valueAge === 16 || valueAge === 17) {
+    console.log('Puedes sacar tu permiso para conducir 😎')
+} else if (valueAge <= 15 || valueAge > 90 ) {
+    console.warn('No puedes conducir 😞')
+}else {
+    console.error('No es una edad valida, intentalo de nuevo 🤬')
+}
+
+
+//-------- Challenge 2 --------
+// Juego de piedra papel o tijera ✂️ ⛰ 📃
+// Preguntar a 2 usuarios que opcion eleguir
+// Mostrar en consola que jugador gano y porque
