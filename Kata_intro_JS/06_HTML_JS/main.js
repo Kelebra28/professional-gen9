52b5faf5fb00d1d9bb5b69c5d1cf3f85e8ce471b
Kelebra <ricardokelebra@gmail.com>
2021-07-07 20:30:49 -0500

    dom

--- Kata_intro_JS/06_HTML_JS/main.js
+++ Kata_intro_JS/06_HTML_JS/main.js
@@ -7,16 +7,21 @@ var parrafo = document.getElementById('parrafo')
 var boton = document.getElementById('boton')
 var input = document.getElementById('input')
 var respuesta = document.getElementById('respuesta')
-
+var card = document.getElementById('card')
+var pizzaImg = document.getElementById('pizza_img')
+var mostrar = true
 // var xd = document.getElementsByClassName()
 
-console.log(titulo)
-console.log(parrafo)
-console.log(boton)
+// console.log(titulo)
+// console.log(parrafo)
+// console.log(boton)
 
 // innerHTML -> pinta en el navegador 
 
-
+// .classList -> me enseña las clases que tiene mi etiquta
+console.log(card.classList)
+// add -> agrega una clase
+// remove -> elimina una clase
 
 function cambio() {
 
@@ -24,6 +29,15 @@ function cambio() {
     titulo.innerHTML = 'Soy el titulo cambiado desde JS'
     parrafo.innerHTML = 'Soy el parrafo cambiado desde JS'
     respuesta.innerHTML = text
+    if( mostrar === true){
+        card.classList.remove('none')
+        mostrar = false
+    }else if (mostrar === false) {
+          card.classList.add('none')
+          mostrar = true
+    }
+  
+    pizzaImg.src = 'https://c8.alamy.com/compes/amgr7n/mexico-ciudad-de-mexico-dominos-pizza-amgr7n.jpg'
 
 }
 
