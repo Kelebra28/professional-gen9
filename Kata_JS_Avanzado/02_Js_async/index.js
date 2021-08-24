17682f73214b10e024d25100cc7ec6218557d564
Kelebra <ricardokelebra@gmail.com>
2021-08-23 21:26:03 -0500

    async

---
+++ Kata_JS_Avanzado/02_Js_async/index.js
@@ -0,0 +1,32 @@
+
+// console.log('----Todo en la pila de ejecucion-----')
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+
+// console.log('-----El 2 y el 3 van a la cola de ejecucion-----')
+// console.log(1)
+// // callback -> funcion como parametro
+// setTimeout(() => {
+//     return console.log(2)
+// }, 3000)
+
+// setTimeout(() => {
+//     return console.log(3)
+// }, 2000)
+
+// console.log(4)
+
+
+// console.log('------ Simulacion de cuello de botella ---------')
+// console.log(1)
+// setTimeout(() => {
+//     return console.log(2)
+// }, 2000)
+
+// for(let i = 0; i <= 1000000000; i++) {
+//     console.log(i, 'xd')
+// }
+
+// console.log(3)
