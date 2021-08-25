6b4b5acdccf8977f6af43f3c87f8c16eca3356f4
Kelebra <ricardokelebra@gmail.com>
2021-08-24 20:28:29 -0500

    ejercicios de callback

--- Kata_JS_Avanzado/02_Js_async/index.js
+++ Kata_JS_Avanzado/02_Js_async/index.js
@@ -30,3 +30,79 @@
 // }
 
 // console.log(3)
+
+
+// ----------------- Ejercicios ------------
+// ----1
+const saludo = (imprimir, mensaje) => {
+    imprimir(mensaje)
+}
+// primero parametro es una funcion -> callback
+// saludo((mensaje) => {
+//     console.log(mensaje)
+// } ,'Hola desde un callback') // el segundo parametro es el mensaje
+
+// ----2
+
+const valor = (queTipo, variable) => {
+    queTipo(variable)
+}
+
+// valor((variable) => {
+//     console.log(typeof(variable))
+// }, 1)
+
+// -----3
+
+const imprimirOperacion = (operacion, x, y) => {
+    operacion(x, y)
+}
+// suma
+// imprimirOperacion((x, y) => {
+//     console.log(x + y)
+// }, 10, 50)
+// // resta
+// imprimirOperacion((x, y) => {
+//         console.log(x - y)
+// }, 50, 10)
+// // multiplicacion
+// imprimirOperacion((x, y) => {
+//     console.log(x * y)
+// }, 10, 10)
+// // division
+// imprimirOperacion((x, y) => {
+//     console.log( x / y)
+// }, 100, 20)
+
+// ----4
+
+const caracteres = (convertir, texto, caso) => {
+    convertir(texto, caso)
+}
+
+// caracteres((texto, caso) => {
+//     if(caso === 'minus'){
+//         console.log(texto.toLowerCase())
+//     } else if ( caso === 'mayus') {
+//         console.log(texto.toUpperCase())
+//     }else {
+//         console.log('No es un caso valido')
+//     } // texto    caso
+// }, 'Ricardo XD', 'mayus')
+
+
+// -----5
+
+const devolverHora = (convertir, tiempos) => {
+    convertir(tiempos)
+}
+
+devolverHora((tiempos) => {
+    // map -> for
+    //   tiempo es cada elemento del arreglo
+    tiempos.map((tiempo) => {
+        if( tiempo / 60 >= 2){
+            console.log(tiempo)
+        }
+    })
+}, [120, 80, 200, 100])
